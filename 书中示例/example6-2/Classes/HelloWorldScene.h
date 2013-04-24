1ab197853befa281f55a2dfb4bc162bf4ebfd21c
manshuoquan <manshuoquan@sina.cn>
2013-04-24 23:37:11 +0800

    书中示例提交
    支持cocos2d-x 2.0.1

---
+++ 书中示例/example6-2/Classes/HelloWorldScene.h
@@ -0,0 +1,28 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::CCLayer
+{
+public:
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();  
+
+    // there's no 'id' in cpp, so we recommand to return the exactly class pointer
+    static cocos2d::CCScene* scene();
+    
+    // a selector callback
+    void menuCloseCallback(CCObject* pSender);
+
+    // implement the "static node()" method manually
+    LAYER_CREATE_FUNC(HelloWorld);
+    
+    virtual void registerWithTouchDispatcher(void);
+    virtual void ccTouchesBegan(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+    virtual void ccTouchesMoved(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+    virtual void ccTouchesEnded(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+    virtual void ccTouchesCancelled(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
